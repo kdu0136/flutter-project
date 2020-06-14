f4d5973738b0d269124023cfb1cd472687778510
dongunkim <kimdongun1127@gmail.com>
2020-06-14 17:18:32 +0900

    - Add social login button

--- daliy_memory/lib/enum.dart
+++ daliy_memory/lib/enum.dart
@@ -1,4 +1,8 @@
 
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
 enum Gender { MAN, WOMAN }
 extension GenderExtension on Gender {
   String getValue() {
@@ -9,6 +13,34 @@ extension GenderExtension on Gender {
         return '여자';
       default:
         return null;
+    }
+  }
+}
+
+enum Social { FACEBOOK, GOOGLE, KAKAO }
+extension SocialExtension on Social {
+  String getSymbolChar() {
+    switch (this) {
+      case Social.FACEBOOK:
+        return 'F';
+      case Social.GOOGLE:
+        return 'G';
+      case Social.KAKAO:
+        return 'K';
+      default:
+        return '';
+    }
+  }
+  List<Color> getSymbolColor() {
+    switch (this) {
+      case Social.FACEBOOK:
+        return [Colors.blue, Colors.blueAccent];
+      case Social.GOOGLE:
+        return [Colors.white, Colors.redAccent];
+      case Social.KAKAO:
+        return [Colors.yellow, Colors.yellowAccent];
+      default:
+        return [];
     }
   }
 }
--- daliy_memory/lib/login.dart
+++ daliy_memory/lib/login.dart
@@ -1,4 +1,6 @@
+import 'package:daliymemory/enum.dart';
 import 'package:daliymemory/register.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatefulWidget {
@@ -19,6 +21,38 @@ class _LoginPageState extends State<LoginPage> {
     _runRegisterPage(context, 'kakao email');
   }
 
+  Widget socialLoginBtn({@required Social social, GestureTapCallback onPress}) {
+    var btnColor = social.getSymbolColor();
+    return ClipOval(
+      child: Material(
+        color: btnColor[0],
+        child: InkWell(
+          splashColor: btnColor.length > 1 ? btnColor[1] : btnColor[0],
+          child: Container(
+            width: 70,
+            height: 70,
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              border: Border.all(
+                width: 3.0,
+                color: Colors.black,
+              )
+            ),
+            child: Center(
+              child: Text(
+                social.getSymbolChar(),
+                style: TextStyle(
+                  fontSize: 30,
+                ),
+              ),
+            ),
+          ),
+          onTap: onPress,
+        ),
+      ),
+    );
+  }
+
   _runRegisterPage(BuildContext context, email) => Navigator.push(
         context,
         MaterialPageRoute(builder: (context) => RegisterPage(email)),
@@ -33,9 +67,11 @@ class _LoginPageState extends State<LoginPage> {
           Expanded(
             child: Center(
               child: Text(
-                '하루 한장',
+                '1234',
                 style: TextStyle(
                   fontSize: 45.0,
+//                  fontFamily: 'Hanna',
+//                  fontWeight: FontWeight.bold
                 ),
               ),
             ),
@@ -45,18 +81,9 @@ class _LoginPageState extends State<LoginPage> {
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: <Widget>[
-                  RaisedButton(
-                    child: Text('구글 로그인'),
-                    onPressed: _googleLogin,
-                  ),
-                  RaisedButton(
-                    child: Text('페이스북 로그인'),
-                    onPressed: _facebookLogin,
-                  ),
-                  RaisedButton(
-                    child: Text('카카오 로그인'),
-                    onPressed: _kakaoLogin,
-                  ),
+                  socialLoginBtn(social: Social.GOOGLE, onPress: _googleLogin),
+                  socialLoginBtn(social: Social.FACEBOOK, onPress: _facebookLogin),
+                  socialLoginBtn(social: Social.KAKAO, onPress: _kakaoLogin),
                 ],
               ),
             ),
