d83ed1886e1e507fcba134bd0cbde224c3e02e62
dongun kim <>
2020-06-26 18:48:02 +0900

    - Add lifecycle override function.

--- flutter_widgets/lib/complicatedUI/servicePage.dart
+++ flutter_widgets/lib/complicatedUI/servicePage.dart
@@ -6,6 +6,45 @@ class ServicePage extends StatefulWidget {
 }
 
 class _ServicePageState extends State<ServicePage> {
+  /// initState
+  /// 위젯이 생성될 때 호출
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    print('${this.runtimeType} initState');
+  }
+
+  @override
+  void didChangeDependencies() {
+    // TODO: implement didChangeDependencies
+    super.didChangeDependencies();
+    print('${this.runtimeType} didChangeDependencies');
+  }
+
+  @override
+  void didUpdateWidget(ServicePage oldWidget) {
+    // TODO: implement didUpdateWidget
+    super.didUpdateWidget(oldWidget);
+    print('${this.runtimeType} didUpdateWidget');
+  }
+
+  @override
+  void deactivate() {
+    // TODO: implement deactivate
+    super.deactivate();
+    print('${this.runtimeType} deactivate');
+  }
+
+  /// dispose
+  /// 위젯이 완전히 종료될 때 호출
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    print('${this.runtimeType} dispose');
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
