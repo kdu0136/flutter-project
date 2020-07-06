d00d1410e8482b19c3c9e04aaec220fe4873403c
dongun kim <>
2020-07-06 17:07:51 +0900

    - Add login & sign in page UI. (https://www.youtube.com/watch?v=PpekJXY04fM)

---
+++ youtube_flutter_UI/flutter_login_screen/lib/constants.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+const kBackgroundColor = MaterialColor(0xFF202020, const <int, Color>{
+  50: const Color(0xFF202020),
+  100: const Color(0xFF202020),
+  200: const Color(0xFF202020),
+  300: const Color(0xFF202020),
+  400: const Color(0xFF202020),
+  500: const Color(0xFF202020),
+  600: const Color(0xFF202020),
+  700: const Color(0xFF202020),
+  800: const Color(0xFF202020),
+  900: const Color(0xFF202020),
+});
+const kPrimaryColor = MaterialColor(0xFFFFBD73, const <int, Color>{
+  50: const Color(0xFFFFBD73),
+  100: const Color(0xFFFFBD73),
+  200: const Color(0xFFFFBD73),
+  300: const Color(0xFFFFBD73),
+  400: const Color(0xFFFFBD73),
+  500: const Color(0xFFFFBD73),
+  600: const Color(0xFFFFBD73),
+  700: const Color(0xFFFFBD73),
+  800: const Color(0xFFFFBD73),
+  900: const Color(0xFFFFBD73),
+});
