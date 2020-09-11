a70a61de87a0837ea2a7176d204a53fd91c9c4e8
dongun kim <>
2020-09-11 18:40:54 +0900

    - Add create room button on sliver.

---
+++ youtube_flutter_UI/flutter_facebook_app/lib/main_app.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_facebook_app/config/palette.dart';
+
+import 'screens/screens.dart';
+
+class MainApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Facebook UI',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+        scaffoldBackgroundColor: Palette.scaffold,
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
